47f65556f69ed440b65435156f682ab66f6baf38
Alok Kamatar <alokvk2@uchicago.edu>
2023-08-03 11:39:33 -0500

    Adding test for attaching to process

--- tests/run.py
+++ tests/run.py
@@ -59,7 +59,7 @@ class TestProfilerMethods(unittest.TestCase):
                     )
                     program.run(sample_period=sp, reset_on_sample=res)
 
-    @run_n(10)
+    @run_n(1)
     def test_runpython(self):
         for sp in [-0.1, 0, 0.1]:
             for res in [True, False]:
@@ -134,6 +134,39 @@ class TestProfilerMethods(unittest.TestCase):
                     program.run_background()
                     while program.program.isAlive:
                         pass
+
+    @run_n(1)
+    def test_attach_to_process(self):
+        import multiprocessing
+        from multiprocessing import Barrier
+
+        def cpu_burn(start_barrier, seconds):
+            import time
+            
+            start_barrier.wait()
+            begin = time.time()
+            while time.time() - begin < seconds:
+                result = 0
+                for i in range(10000):
+                    result += i
+
+
+        for sp in [-0.1, 0, 0.1]:
+            for res in [True, False]:
+                start_barrier = Barrier(2)
+                process = multiprocessing.Process(target=cpu_burn, args=(start_barrier, 1))
+                process.start()
+
+                program = Profiler(
+                    pid = process.pid
+                    events_groups=[["PERF_COUNT_HW_INSTRUCTIONS"]],
+                )
+                self.assertTrue(
+                    program.run_python(sample_period=sp, reset_on_sample=res)
+                )
+
+
+
 
 
 if __name__ == "__main__":
